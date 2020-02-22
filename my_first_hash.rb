4a6224c690b4e98c8958d719778e8f8b832d892e
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 07:15:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                  }
 shipping_manifest["muskets"] = 2 
-
+shipping_manifest["muskets"] = 2 
 
 
   # add 4 gun powder to the shipping_manifest hash below
